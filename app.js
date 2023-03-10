4cb0816a77cc981b2a74293caba441b5f09fb586
MD. Hazzaz Bin Faiz <hazzazbinfaiz@gmail.com>
2023-03-11 01:53:11 +0600

    Use raw parser

--- app.js
+++ app.js
@@ -12,12 +12,26 @@ const csrfProtection = csrf({ cookie: true });
 const app = express();
 require('./src/api/v1/config').dbConnection();
 
+app.use(function(req, res, next) {
+  var contentType = req.headers['content-type'] || ''
+    , mime = contentType.split(';')[0];
+
+  if (mime != 'application.json') {
+    return next();
+  }
+
+  var data = '';
+  req.setEncoding('utf8');
+  req.on('data', function(chunk) {
+    data += chunk;
+  });
+  req.on('end', function() {
+    req.rawBody = data;
+    next();
+  });
+});
+
 app.use(express.json());
-app.use(express.raw({
-  inflate: true,
-  limit: '100kb',
-  type: 'application/json'
-}));
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
