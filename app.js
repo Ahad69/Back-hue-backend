d35346957a95243b8cd893fd7ddb44bedb5609dc
Ahad69 <ahadxx99@gmail.com>
2023-04-19 17:11:42 +0600

    updated

--- app.js
+++ app.js
@@ -19,10 +19,10 @@ app.use(express.json({
   limit : "5mb"
 }));
 app.use(express.urlencoded({ extended: false , limit: '5mb'}));
-app.use(cors());
-// app.use(cors({
-//   origin: 'http://localhost:3000/'
-// }));
+// app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
 // app.use(express.json())
 
 
