c026f2d60f8d2ce5df0abd12b3289ce4f49927cb
Ahad69 <ahadxx99@gmail.com>
2023-05-02 19:09:32 +0600

    updated

--- app.js
+++ app.js
@@ -22,13 +22,14 @@ app.use(
 app.use(express.urlencoded({ extended: false, limit: "5mb" }));
 
 // app.use(cors());
-app.use(
-  cors({
-    origin: '*',
- })
-);
-
-
+const corsOptions ={
+  origin: "*", 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+  
+}
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions))
 
 
 app.use(cookieParser());
