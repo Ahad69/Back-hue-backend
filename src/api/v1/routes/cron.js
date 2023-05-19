abf8021d1679a7d56d39cbca5e199adb1bd4ec09
Ahad69 <ahadxx99@gmail.com>
2023-05-19 14:44:15 +0600

    updated

--- src/api/v1/routes/cron.js
+++ src/api/v1/routes/cron.js
@@ -4,28 +4,24 @@ const { Product } = require("../models");
 const router = require("express").Router();
 
 router.get("/", async (req, res) => {
-  try {
-    const updateDataStatus = async () => {
-      const validate = await Product.find({
-        $and: [
-          { isDelete: false },
-          { premiumDay: { $gt: 0 } },
-          { $project: { name: 1, premiumDay: 1, isPremium: 1 } },
-        ],
-      });
+  const updateDataStatus = async () => {
+    const validate = await Product.find({
+      $and: [
+        { isDelete: false },
+        { premiumDay: { $gt: 0 } },
+        { $project: { name: 1, premiumDay: 1, isPremium: 1 } },
+      ],
+    });
 
-      const minus = validate.map(async (a) => {
-        a.premiumDay = a.premiumDay - 12;
-        if (a.premiumDay == 0) {
-          a.isPremium = true;
-        }
-        await a.save();
-      });
-    };
-    await updateDataStatus();
-  } catch (error) {
-    res.send(error)
-  }
+    const minus = validate.map(async (a) => {
+      a.premiumDay = a.premiumDay - 12;
+      if (a.premiumDay == 0) {
+        a.isPremium = true;
+      }
+      await a.save();
+    });
+  };
+  await updateDataStatus();
 });
 
 module.exports = router;
