b5018297ac5160ddf01b88c59bd7cb845d0c1b38
Ahad69 <ahadxx99@gmail.com>
2023-12-17 13:43:25 +0600

    updated

--- src/api/v1/transaction/Model.js
+++ src/api/v1/transaction/Model.js
@@ -3,11 +3,12 @@ const mongoose = require('mongoose');
 
 const transactionSchema = Schema(
   {
-    isCompleted : { type: String },
-    date : { type : String },
-    invoice : { type: String, trim: true },
-    amount : {type: Number , default : 0},
-    userId : {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    isCompleted: { type: String },
+    date: { type: String },
+    invoice: { type: String, trim: true },
+    amount: { type: Number, default: 0 },
+    exactAmount: { type: Number, default: 0 },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     isDelete: { type: Boolean, default: false },
   },
   { timestamps: true }
